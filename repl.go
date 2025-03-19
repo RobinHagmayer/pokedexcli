aaf6fddaa31e3a510cb7367823a075344b1717e9
RobinHagmayer <robin.hagmayer@proton.me>
2025-03-19 11:19:13 +0100

    feat: implemented basic REPL

--- repl.go
+++ repl.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func StartREPL() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
