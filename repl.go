40263863391737eb9636cfd6557a0a7505f98bd2
RobinHagmayer <robin.hagmayer@proton.me>
2025-03-20 10:46:28 +0100

    feat: implemented commands logic and first commands

--- repl.go
+++ repl.go
@@ -7,8 +7,10 @@ import (
 	"strings"
 )
 
-func cleanInput(text string) []string {
-	return strings.Fields(strings.ToLower(text))
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
 }
 
 func StartREPL() {
@@ -17,12 +19,60 @@ func StartREPL() {
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		words := cleanInput(scanner.Text())
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		cmdName := input[0]
 
-		if len(words) == 0 {
+		cliCommands := getCliCommands()
+		cmd, ok := cliCommands[cmdName]
+		if !ok {
+			fmt.Println("Unknown command")
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", words[0])
+		err := cmd.callback()
+		if err != nil {
+			errMsg := fmt.Errorf("error after calling %s: %w", cmd.name, err)
+			fmt.Println(errMsg)
+			continue
+		}
 	}
+}
+
+func getCliCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, cmd := range getCliCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
