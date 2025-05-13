0e9ca6727735473e7ecd05a1fd9462ad35823473
Robin Hagmayer <robin.hagmayer@proton.me>
2025-05-13 21:37:45 +0200

    feat: implemented caching of HTTP responses

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -5,12 +5,52 @@ import (
 	"time"
 )
 
+type Cache struct {
+	cache map[string]cacheEntry
+	mux   *sync.Mutex
+}
+
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
 }
 
-type Cache struct {
-	locationAreaData map[string]cacheEntry
-	mu               sync.Mutex
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
+	}
+
+	go c.reapLoop(interval)
+
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mux.Lock()
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.mux.Unlock()
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	cache, ok := c.cache[key]
+	c.mux.Unlock()
+	return cache.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.mux.Lock()
+		for key, cache := range c.cache {
+			if time.Since(cache.createdAt) > interval {
+				delete(c.cache, key)
+			}
+		}
+		c.mux.Unlock()
+	}
 }
